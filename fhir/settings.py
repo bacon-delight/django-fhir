670c0978946c391e2aecd28a3f61dd4d1ee61d85
Dipanjan <dipanjan131@gmail.com>
2023-02-22 14:03:40 +0530

    Updated config for Vercel

--- fhir/settings.py
+++ fhir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$$0rtl$mshf=fek$oi_8a5-@j2$!u+nhs^!5^(o5fods4^h+p=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition

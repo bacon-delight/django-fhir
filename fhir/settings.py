949bdbe85517d4af64c729727c88d1b171da746e
Dipanjan <dipanjan131@gmail.com>
2023-02-22 14:10:44 +0530

    Updated Requirements

--- fhir/settings.py
+++ fhir/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

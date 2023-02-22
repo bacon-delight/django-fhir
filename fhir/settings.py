713580c19a0b5c372807385323b836ecaf706762
Dipanjan <dipanjan131@gmail.com>
2023-02-22 20:08:54 +0530

    Removed Debug Modes

--- fhir/settings.py
+++ fhir/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$$0rtl$mshf=fek$oi_8a5-@j2$!u+nhs^!5^(o5fods4^h+p="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if env("MODE") == "DEVELOPMENT" else False
+DEBUG = True
 
 ALLOWED_HOSTS = json.loads(env("ALLOWED_HOSTS"))
 

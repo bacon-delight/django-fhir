f71f13a4dd6f1363edc1f9a58a0fd2224751a8e8
Dipanjan <dipanjan131@gmail.com>
2023-02-22 14:30:55 +0530

    Updated Allowed Hosts

--- fhir/settings.py
+++ fhir/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import mimetypes
+# import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
+# mimetypes.add_type("text/css", ".css", True)
 
 from pathlib import Path
+import json
+from env import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +31,7 @@ SECRET_KEY = "django-insecure-$$0rtl$mshf=fek$oi_8a5-@j2$!u+nhs^!5^(o5fods4^h+p=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = json.loads(env("ALLOWED_HOSTS"))
 
 
 # Application definition

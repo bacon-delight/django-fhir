ff790a3acf7a8c09db5bd6d139914dbed8b629d1
Dipanjan <dipanjan131@gmail.com>
2023-02-23 10:12:28 +0530

    Updated Search Option

--- baseR4/resources/Patient/search.py
+++ baseR4/resources/Patient/search.py
@@ -1,9 +1,10 @@
 def create_search_options(params):
     options = {}
+    keys = params.keys()
 
-    if params["active"]:
+    if "active" in keys:
         options["active"] = True if params["active"] == "true" else False
-    if params["gender"]:
+    if "gender" in keys:
         options["gender"] = params["gender"]
 
     return options
